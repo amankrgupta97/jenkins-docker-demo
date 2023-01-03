451fe35c9a34bc9ac496f78aab4fa74a8acf1cbc
Aman Gupta <amankrgupta97@gmail.com>
2023-01-03 13:59:21 +0530

    jenkins updated

---
+++ jenkinsDockerDemo/src/main/java/com/nagarro/jenkinsDockerDemo/model/Employee.java
@@ -0,0 +1,26 @@
+package com.nagarro.jenkinsDockerDemo.model;
+
+public class Employee {
+	
+	public int id;
+	public String name;
+	public Employee(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
